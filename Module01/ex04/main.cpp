cf2cba1fd3c0bca2a3459793e39074ef616a5acc
SpaMillie <militsaspasic@gmail.com>
2024-11-09 16:43:19 +0200

    read the subject mainly

--- Module01/ex04/main.cpp
+++ Module01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mspasic <mspasic@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 23:01:45 by mspasic           #+#    #+#             */
-/*   Updated: 2024/11/08 23:05:57 by mspasic          ###   ########.fr       */
+/*   Updated: 2024/11/09 16:42:52 by mspasic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,17 @@
 
 int main(int argc, char **argv)
 {
-    
+   //takes three parameters: a filename + 2 strs s1 and s2
+   //open filename and copy its content into a new file filename.replace
+   //but if there is s1 somewhere it is replaced by s2
+   //cant use string::replace
+   //handle errors and unexpected inputs
+   //ifstream, ofstream, fstream
+
+    if (argc == 4)
+    {
+        //code
+    }
+    else
+        std::cout << "Error: Wrong number of arguments given. Please provide 3 arguments.\n";
 }
