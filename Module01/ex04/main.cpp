11915087a353090a51d6873d9ea8a8cfe6af7827
Milica <militsaspasic@gmail.com>
2024-11-13 16:33:22 +0200

    made some progress on this finally

--- Module01/ex04/main.cpp
+++ Module01/ex04/main.cpp
@@ -6,11 +6,37 @@
 /*   By: mspasic <mspasic@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 23:01:45 by mspasic           #+#    #+#             */
-/*   Updated: 2024/11/09 16:42:52 by mspasic          ###   ########.fr       */
+/*   Updated: 2024/11/13 16:28:27 by mspasic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
+#include "Streaming.hpp"
+
+void write_error(std::string str)
+{
+    std::cout << str;
+}
+
+int start_this(Streaming *cur)
+{
+    std::fstream filename;
+    std::fstream repl_filename;
+    std::string repl_str = ".replace";
+
+    filename.open(cur->get_element(1), std::ios::in);
+    if (!filename.is_open())
+    {
+        write_error("Error: Cannot access file.\n");
+        return (-1);
+    }
+    repl_filename.open(cur->get_element(1) + repl_str, std::ios::out);
+    // cur->set_element(*argv, 1);
+
+    std::cout << " got here\n";
+    filename.close();
+    repl_filename.close();
+    return (0);
+}
 
 int main(int argc, char **argv)
 {
@@ -20,10 +46,16 @@ int main(int argc, char **argv)
    //cant use string::replace
    //handle errors and unexpected inputs
    //ifstream, ofstream, fstream
+    // int check = 0;
+    Streaming cur;
 
     if (argc == 4)
     {
-        //code
+        cur.set_element(argv[1], 1);
+        cur.set_element(argv[2], 2);
+        cur.set_element(argv[3], 3);
+        if (start_this(&cur) == -1)
+            return (1);
     }
     else
         std::cout << "Error: Wrong number of arguments given. Please provide 3 arguments.\n";
