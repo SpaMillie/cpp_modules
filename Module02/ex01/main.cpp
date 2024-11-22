efe8373b1f9c2413c22f9af85bfebcf2159d1944
Mils <militsaspasic@gmail.com>
2024-11-22 13:15:43 +0200

    added tests for the other way of doing the calculation

--- Module02/ex01/main.cpp
+++ Module02/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mspasic <mspasic@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 16:39:23 by mspasic           #+#    #+#             */
-/*   Updated: 2024/11/21 17:06:33 by mspasic          ###   ########.fr       */
+/*   Updated: 2024/11/22 13:14:43 by mspasic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ int main( void ) {
 
     a = Fixed( 1234.4321f );
 
+    // std::cout.precision(2);
+
     std::cout << "a is " << a << std::endl;
     std::cout << "b is " << b << std::endl;
     std::cout << "c is " << c << std::endl;
@@ -29,9 +31,12 @@ int main( void ) {
     std::cout << "b is " << b.toInt() << " as integer" << std::endl;
     std::cout << "c is " << c.toInt() << " as integer" << std::endl;
     std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+
+    // std::cout << std::fixed;
     std::cout << "a is " << a.toFloat() << " as float" << std::endl;
     std::cout << "b is " << b.toFloat() << " as float" << std::endl;
     std::cout << "c is " << c.toFloat() << " as float" << std::endl;
     std::cout << "d is " << d.toFloat() << " as float" << std::endl;
+
     return 0;
 }
