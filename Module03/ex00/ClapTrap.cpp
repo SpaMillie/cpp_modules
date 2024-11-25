a7b8ba413ff5f14ca97c14c67934b605fd0585e8
Mils <militsaspasic@gmail.com>
2024-11-25 14:37:23 +0200

    started module03, looking into claptraps

---
+++ Module03/ex00/ClapTrap.cpp
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mspasic <mspasic@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/25 13:24:40 by mspasic           #+#    #+#             */
+/*   Updated: 2024/11/25 14:36:23 by mspasic          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap():name(" "), hit_pts(10), energy_pts(10), attack_dmg(0){}
+
+ClapTrap::ClapTrap(std::string name):name(name), hit_pts(10), energy_pts(10), attack_dmg(0){}
+
+ClapTrap::ClapTrap(const ClapTrap& obj){
+    *this = obj;
+}
+
+ClapTrap& ClapTrap::operator=(const ClapTrap& other){
+    if (this == &other)
+        return (*this);
+    this->name = other.name;
+    this->hit_pts = other.hit_pts;
+    this->energy_pts = other.energy_pts;
+    this->attack_dmg = other.attack_dmg;
+    return (*this);
+}
+
+ClapTrap::~ClapTrap(){
+    std::cout << "Destructor called\n";
+}
+
+void    ClapTrap::attack(const std::string& target){
+    std::cout << "ClapTrap " << this->name << " attacks " << target <<  "causing <damage> points of damage!\n";
+}
+
+void    ClapTrap::takeDamage(unsigned int amount){
+
+}
+
+void    ClapTrap::beRepaired(unsigned int amount){
+
+}
+
+
+std::string ClapTrap::get_name(void){
+    return this->name;
+}
+
+unsigned int ClapTrap::get_int(int whichone){
+    switch(whichone){
+        case 1:
+            return this->hit_pts;
+        case 2:
+            return this->energy_pts;
+        case 3:
+            return this->attack_dmg;
+    }
+}
---
+++ Module03/ex00/ClapTrap.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mspasic <mspasic@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/25 13:23:21 by mspasic           #+#    #+#             */
+/*   Updated: 2024/11/25 14:31:29 by mspasic          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+# include <iostream>
+
+class ClapTrap {
+    private:
+        std::string name;
+        unsigned int hit_pts; //camel or snake??????
+        unsigned int energy_pts;
+        unsigned int attack_dmg;
+    public:
+        ClapTrap();
+        ClapTrap(std::string name);
+        ClapTrap(const ClapTrap& obj);
+        ClapTrap& operator=(const ClapTrap& other);
+        ~ClapTrap();
+        void            attack(const std::string& target);
+        void            takeDamage(unsigned int amount);
+        void            beRepaired(unsigned int amount);
+        std::string     get_name(void);
+        unsigned int    get_int(void);
+};
+
+#endif
