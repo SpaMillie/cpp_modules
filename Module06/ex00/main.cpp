50471f4702d86d2d2bd752c80c122f54dfe3b661
Mils <militsaspasic@gmail.com>
2025-01-10 15:38:29 +0200

    added some more tests

--- Module06/ex00/main.cpp
+++ Module06/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mspasic <mspasic@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/06 16:25:46 by mspasic           #+#    #+#             */
-/*   Updated: 2025/01/08 19:33:15 by mspasic          ###   ########.fr       */
+/*   Updated: 2025/01/10 15:36:58 by mspasic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,14 +61,39 @@ int main(void){{
     std::cout << std::endl;
     test->convert("42.4095800f"); //some zeroes at the end of the fractional part
     std::cout << std::endl;
-    test->convert("324324.324324"); //no zeroes
-    std::cout << std::endl;
+    std::cout << "*************************\n";
+    std::cout << "TESTING: THE 'WEIRD' STUFF\n";
+    {
+        test->convert("324324.324324"); //no zeroes
+        double num = 324324.324324;
+        float num2 = static_cast <float> (num);
+        std::cout << std::fixed << std::setprecision(6) << "the double: " << num << "\nthe float: " << num2 << "\n\n";
+    }
     test->convert("324324.324324f"); // no zeroes but an f
     std::cout << std::endl;
-    test->convert("12345.67890123456789"); // long double
-    std::cout << std::endl;
-    test->convert("inf");
-    std::cout << std::endl;
+    {
+        /*a char holds 1 byte while an int holds 4 so when typecast, the int is trunc-ed
+        (the higher order bits get discarded)
+        int 1234 = 0011 0000 0011 1001
+        only the least significant 8 bits remain
+        => 0011 1001 = 57
+        char type with the value of 57 is an existing ascii char*/
+        
+        test->convert("12345.67890123456789"); // long double or int that results in a valid character
+        double num = 12345.67890123456789;
+        int num2 = 12345;
+        char c = (char)num;
+        std::cout << "check: " << c << std::endl;
+        c = (char)num2;
+        std::cout << "check: " << c << std::endl;
+    }
+    {
+        test->convert("inf");
+        double num = std::numeric_limits<double>:: infinity();
+        int num2 = static_cast <int> (num);
+        std::cout << "check: " << num << std::endl;
+        std::cout << "check: " << num2 << std::endl;
+    }
     test->convert("nanf");
     std::cout << std::endl;
     test->convert("-inf");
@@ -85,23 +110,5 @@ int main(void){{
     float num = .324f;
     std::cout << std::fixed << std::setprecision(6);
     std::cout << "check: " << num << std::endl;
-}
-{
-    double num = 324324.324324;
-    float check = (float) num;
-    std::cout << "check: " << check << std::endl;
-}
-{
-    float num = 42.0f;
-    char c = (char)num;
-    std::cout << "check: " << c << std::endl;
-}
-{
-    double num = 12345.67890123456789;
-    int num2 = 12345;
-    char c = (char)num;
-    std::cout << "check: " << c << std::endl;
-    c = (char)num2;
-    std::cout << "check: " << c << std::endl;
 }
 }
