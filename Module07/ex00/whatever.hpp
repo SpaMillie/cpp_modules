8ab83ac8da915e446bddec2f95038a73ffa0ccd8
Mils <militsaspasic@gmail.com>
2025-03-19 20:27:37 +0200

    did ex00 mod07

---
+++ Module07/ex00/whatever.hpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mspasic <mspasic@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/19 20:10:49 by mspasic           #+#    #+#             */
+/*   Updated: 2025/03/19 20:24:10 by mspasic          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+template <typename T> void swap(T& x, T& y){
+    T temp = x;
+    x = y;
+    y = temp;
+}
+
+template <typename T> T min(T x, T y){
+    //shortest: return ((y < x) ? y : x);
+    if (y <= x)
+        return y;
+    return x;
+}
+
+template <typename T> T max(T x, T y){
+    //shortest: return ((y > x) ? y : x);
+    if (y >= x)
+        return y;
+    return x;
+}
