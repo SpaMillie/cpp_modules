ee138f0b52a303261a319bae94c261a884620f11
Mils <militsaspasic@gmail.com>
2025-03-19 20:35:00 +0200

    started ex01

--- Module07/ex01/main.cpp
+++ Module07/ex01/main.cpp
@@ -5,26 +5,11 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mspasic <mspasic@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/19 20:10:07 by mspasic           #+#    #+#             */
-/*   Updated: 2025/03/19 20:29:24 by mspasic          ###   ########.fr       */
+/*   Created: 2025/03/19 20:30:04 by mspasic           #+#    #+#             */
+/*   Updated: 2025/03/19 20:30:29 by mspasic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "whatever.hpp"
+#include "iter.hpp"
 #include <iostream>
-//ADD TESTS
-int main( void ) {
-    int a = 2;
-    int b = 3;
-    ::swap( a, b );
-    std::cout << "a = " << a << ", b = " << b << std::endl;
-    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
-    std::string c = "chaine1";
-    std::string d = "chaine2";
-    ::swap(c, d);
-    std::cout << "c = " << c << ", d = " << d << std::endl;
-    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
-    return 0;
-}
+
