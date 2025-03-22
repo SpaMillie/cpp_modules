7245013a382d92ce49f4025f62fcc8d623c9289d
Mils <militsaspasic@gmail.com>
2025-03-22 15:46:34 +0200

    fixed the scalar convertor, now tis an ultimate scalar convertor :P

--- Module06/ex00/ScalarConverter.cpp
+++ Module06/ex00/ScalarConverter.cpp
@@ -6,13 +6,14 @@
 /*   By: mspasic <mspasic@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/06 15:17:38 by mspasic           #+#    #+#             */
-/*   Updated: 2025/03/18 15:01:40 by mspasic          ###   ########.fr       */
+/*   Updated: 2025/03/22 15:42:28 by mspasic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 #include <iomanip>
 #include <iostream>
+#include <limits.h>
 
 /* HELPER FUNCTIONS FOR PRINTING */
 
@@ -53,16 +54,36 @@ bool    flt_check_edge(std::string literal){
 }
 
 bool    dbl_check_edge(std::string literal){
-    if (literal == "inf" || literal == "nan" || literal == "-inf" || literal == "+inf" )
+    if (literal == "nan" || literal == "-inf" || literal == "+inf" )
         return (true);
     return (false);
+}
+
+void    print_edge(std::string lit, char c){
+    float f;
+    double d;
+
+    std::cout << "  character: impossible\n";
+    std::cout << "  integer: impossible\n";
+    if (c == 'f'){
+        f = std::stof(lit);
+        d = static_cast <double> (f);
+        std::cout << "  float: " << f << "f\n";
+        std::cout << "  double: " << d << "\n";
+    }
+    else{
+        d = std::stod(lit);
+        f = static_cast <float> (d);
+        std::cout << "  float: " << f << "f\n";
+        std::cout << "  double: " << d << "\n";
+    }
 }
 
 /* HELPER FUNCTIONS FOR TYPE RECOGNITION */
 
 bool is_char(std::string lit){
-    if ((lit.size() == 1 && (lit[0] >= 0 && lit[0] <= 127)))
-        return (true);
+    if ((lit.size() == 1) && ((lit[0] >= 0 && lit[0] < '0') || (lit[0] > '9' && lit[0] <= 127)))
+            return (true);
     return (false);
 }
 
@@ -124,7 +145,7 @@ bool is_double(std::string lit){
 
 /* SETTING AND PRINTING FUNCTIONS */
 
-void    print_char(char c){
+void    print_char(unsigned char c){
     if ((c >= 0 && c < 32) || c == 127)
         std::cout << "  character: Non displayable\n";
     else if (c > 31 && c < 127)
@@ -133,7 +154,7 @@ void    print_char(char c){
         std::cout << "  character: impossible\n";
 }
 
-void    print_all(char c, int i, float f, double d, size_t n){
+void    print_all(unsigned char c, int i, float f, double d, size_t n){
     print_char(c);
     std::cout << "  integer: " << i << "\n";
     std::cout << std::fixed << std::setprecision(n);
@@ -142,7 +163,7 @@ void    print_all(char c, int i, float f, double d, size_t n){
 }
 
 void    set_char(std::string literal){
-    char    c = literal[0];
+    unsigned char    c = literal[0];
     int     int_c = static_cast <int> (c);
     float   float_c = static_cast <float> (c);
     double  double_c = static_cast <double> (c);
@@ -152,13 +173,16 @@ void    set_char(std::string literal){
 
 void    set_print_int(std::string literal){
     int     i;
-    char    char_i;
+    unsigned char    char_i;
     float   float_i;
     double  double_i;
 
     try{
         i = std::stoi(literal);
-        char_i = static_cast <char> (i);
+        if (i < 1 || i > 127)
+            char_i = 128;
+        else
+            char_i = static_cast <unsigned char> (i);
         float_i = static_cast <float> (i);
         double_i = static_cast <double> (i);
 
@@ -171,20 +195,24 @@ void    set_print_int(std::string literal){
 
 void    set_print_float(std::string literal){
     int     int_f;
-    char    char_f;
+    unsigned char    char_f;
     float   f;
     double  double_f;
 
     try{
+        if (flt_check_edge(literal) == true){
+            print_edge(literal, 'f');
+            return ;} 
         f = std::stof(literal);
-        char_f = static_cast <char> (f);
-        int_f = static_cast <int> (f);
+        if (f < 1 || f > 127)
+            char_f = 128;
+        else 
+            char_f = static_cast <unsigned char> (f);
         double_f = static_cast <double> (f);
+        if (double_f < INT_MIN || double_f > INT_MAX)
+            return (print_edge)(literal, 'f');
+        int_f = static_cast <int> (f);
         print_all(char_f, int_f, f, double_f, find_dot(literal));
-
-        // std::cout << std::fixed << std::setprecision(find_dot(literal));
-        // std::cout << "  float: " << f << "f\n  character: " << char_f << "\n  integer: ";
-        // std::cout << int_f << "\n  double: " << double_f << "\n";
     }
     catch(const std::exception& e){
        print_invalid(); 
@@ -193,20 +221,33 @@ void    set_print_float(std::string literal){
 
 void    set_print_double(std::string literal){
     int     int_d;
-    char    char_d;
+    unsigned char    char_d;
     float   float_d;
     double  d;
 
     try{
+        if (dbl_check_edge(literal) == true){
+            print_edge(literal, 'd');
+            return ;}
         d = std::stod(literal);
-        char_d = static_cast <char> (d);
+        try{float_d = std::stof(literal);
+        }
+        catch(std::exception& e){
+            std::cout << "  character: impossible\n";
+            std::cout << "  integer: impossible\n";
+            std::cout << "  float: impossible\n";
+            std::cout << "  double: " << d << "\n";
+            return;
+        }
+        if (d < 1 || d > 127)
+            char_d = 128;
+        else  
+            char_d = static_cast <unsigned char> (d);
+        if (d < INT_MIN || d > INT_MAX)
+            return (print_edge)(literal, 'd'); 
         int_d = static_cast <int> (d);
         float_d = static_cast <float> (d);
-        print_all(char_d, int_d, float_d, d, find_dot(literal));
-        
-        // std::cout << std::fixed << std::setprecision(find_dot(literal));
-        // std::cout << "  double: " << d << "\n  character: " << char_d << "\n  integer: ";
-        // std::cout << int_d << "\n  float: " << float_d << "f\n";
+        print_all(char_d, int_d, float_d, d, find_dot(literal)); 
     }
     catch(const std::exception& e){
        print_invalid(); 
@@ -218,10 +259,16 @@ void    set_print_double(std::string literal){
 
 void ScalarConverter::convert(std::string literal){
     std::cout << "Converting the string: \"" << literal << "\" >>\n";
-    if (!literal.empty() && is_int(literal))
-        set_print_int(literal);
-    else if (!literal.empty() && is_char(literal))
-        set_char(literal);
+    if  (!literal.empty() && is_char(literal))
+        set_char(literal); 
+    else if (!literal.empty() && is_int(literal)){
+        try{
+            std::stoi(literal);
+            set_print_int(literal); 
+        }
+        catch(std::exception& e){
+            set_print_double(literal);
+        }}
     else if (!literal.empty() && is_double(literal))
         set_print_double(literal);
     else if (!literal.empty() && is_float(literal))
--- Module06/ex00/main.cpp
+++ Module06/ex00/main.cpp
@@ -6,14 +6,12 @@
 /*   By: mspasic <mspasic@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/06 16:25:46 by mspasic           #+#    #+#             */
-/*   Updated: 2025/03/18 14:45:15 by mspasic          ###   ########.fr       */
+/*   Updated: 2025/03/22 15:37:52 by mspasic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 #include <iostream>
-// #include <iomanip>
-// #include <limits> 
 #include <string>
 
 int main(int argc, char **argv){
@@ -55,9 +53,9 @@ int main(int argc, char **argv){
 //     std::cout << std::endl;
 //     test->convert("2433234"); //longish integer
 //     std::cout << std::endl;
-//     test->convert("243323435346"); //too long of an integer
+//     test->convert("243323435346"); //too long of an integer == double
 //     std::cout << std::endl;
-//     test->convert("-243323435346"); //too long of a negative integer
+//     test->convert("-243323435346"); //too long of a negative integer == double
 //     std::cout << std::endl;
 //     test->convert("243323435346L"); //long integer
 //     std::cout << "*************************\n";
@@ -78,52 +76,17 @@ int main(int argc, char **argv){
 //     std::cout << std::endl;
 //     std::cout << "*************************\n";
 //     std::cout << "TESTING: THE 'WEIRD' STUFF\n";
-//     {
-//         test->convert("324324.324324"); //no zeroes
-//         double num = 324324.324324;
-//         float num2 = static_cast <float> (num);
-//         std::cout << std::fixed << std::setprecision(6) << "the double: " << num << "\nthe float: " << num2 << "\n\n";
-//     }
 //     test->convert("324324.324324f"); // no zeroes but an f
 //     std::cout << std::endl;
-    // {
-        /*a char holds 1 byte while an int holds 4 so when typecast, the int is trunc-ed
-        (the higher order bits get discarded)
-        int 1234 = 0011 0000 0011 1001
-        only the least significant 8 bits remain
-        => 0011 1001 = 57
-        char type with the value of 57 is an existing ascii char*/
-        
-//         test->convert("12345.67890123456789"); // long double or int that results in a valid character
-//         double num = 12345.67890123456789;
-//         int num2 = 12345;
-//         char c = (char)num;
-//         std::cout << "check: " << c << std::endl;
-//         c = (char)num2;
-//         std::cout << "check: " << c << std::endl;
-//     }
-//     {
-//         test->convert("inf");
-//         double num = std::numeric_limits<double>:: infinity();
-//         int num2 = static_cast <int> (num);
-//         std::cout << "check: " << num << std::endl;
-//         std::cout << "check: " << num2 << std::endl;
-//     }
-//     test->convert("nanf");
+//     test->convert("nan");
 //     std::cout << std::endl;
 //     test->convert("-inf");
 //     std::cout << std::endl;
 //     test->convert("+inf");
-// }
-
-// {
-//     float num = std::numeric_limits<float>::infinity();
-//     std::cout << std::fixed << std::setprecision(6);
-//     std::cout << "check: " << num << std::endl;
-// }
-// {
-//     float num = .324f;
-//     std::cout << std::fixed << std::setprecision(6);
-//     std::cout << "check: " << num << std::endl;
+//     test->convert("nanf");
+//     std::cout << std::endl;
+//     test->convert("-inff");
+//     std::cout << std::endl;
+//     test->convert("+inff");
 // }
 }
