2e99fcc7322b801d314698f77c813e1027e26d7a
Mils <militsaspasic@gmail.com>
2025-03-26 19:43:03 +0200

    did 07_01

--- Module07/ex01/iter.hpp
+++ Module07/ex01/iter.hpp
@@ -6,12 +6,18 @@
 /*   By: mspasic <mspasic@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 20:29:53 by mspasic           #+#    #+#             */
-/*   Updated: 2025/03/19 20:33:54 by mspasic          ###   ########.fr       */
+/*   Updated: 2025/03/26 19:35:10 by mspasic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
-template <typename T> void iter (T address[], unsigned length, void* function){
+#include <iostream>
 
+template <typename T, typename F> void iter (T *array, unsigned length, F func){
+    if (array == nullptr)
+        throw std::invalid_argument("Invalid");
+    for (unsigned i = 0; i < length; i++){
+        func(array[i]);
+    }
 }
--- Module07/ex01/main.cpp
+++ Module07/ex01/main.cpp
@@ -6,10 +6,26 @@
 /*   By: mspasic <mspasic@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 20:30:04 by mspasic           #+#    #+#             */
-/*   Updated: 2025/03/19 20:30:29 by mspasic          ###   ########.fr       */
+/*   Updated: 2025/03/26 19:38:12 by mspasic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
-#include <iostream>
+// #include <iostream>
 
+void print_c(char c){std::cout << c << std::endl;}
+
+void print_n(int c){std::cout << c << std::endl;}
+
+void add(int& x){ x += 1;}
+
+int main(void){
+    {
+        char array[] = {'d', 'f', 'd', 't'};
+        int array2[] = {1, 2, 3, 4};
+        ::iter(array, 4, &print_c);
+        ::iter(array2, 4, &print_n);
+        ::iter(array2, 4, &add);
+        ::iter(array2, 4, &print_n);
+    }
+}
