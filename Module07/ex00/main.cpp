1e23a9a874ba32535a3e72d22b4398b5b59d4581
Milica <militsaspasic@gmail.com>
2025-04-19 09:58:59 +0300

    added some tests

--- Module07/ex00/main.cpp
+++ Module07/ex00/main.cpp
@@ -6,25 +6,52 @@
 /*   By: mspasic <mspasic@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 20:10:07 by mspasic           #+#    #+#             */
-/*   Updated: 2025/03/19 20:29:24 by mspasic          ###   ########.fr       */
+/*   Updated: 2025/04/19 09:57:00 by mspasic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "whatever.hpp"
 #include <iostream>
-//ADD TESTS
-int main( void ) {
-    int a = 2;
-    int b = 3;
+
+int main( void ) {{
+    std::cout << "TESTING: CHARACTER\n";
+    char a = 'c';
+    char b = 'i';
+    std::cout << "a = " << a << ", b = " << b << std::endl;
     ::swap( a, b );
     std::cout << "a = " << a << ", b = " << b << std::endl;
     std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
     std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+    std::cout << "*************************\n";
+}{
+    std::cout << "TESTING: INTEGER\n";
+    int a = 2;
+    int b = 3;
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    ::swap( a, b );
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+    std::cout << "*************************\n";
+}{
+    std::cout << "TESTING: STRING\n";
     std::string c = "chaine1";
     std::string d = "chaine2";
+    std::cout << "c = " << c << ", d = " << d << std::endl;
     ::swap(c, d);
     std::cout << "c = " << c << ", d = " << d << std::endl;
     std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
     std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
-    return 0;
+    std::cout << "*************************\n";
+}{
+    std::cout << "TESTING: FLOAT\n";
+    float c = 453.32f;
+    float d = 83498.2f;
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+    std::cout << "*************************\n";
+}
 }
