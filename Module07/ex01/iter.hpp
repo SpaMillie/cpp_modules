3e187a3e250d747057f6037940e0c1d10b4c800e
Milica <militsaspasic@gmail.com>
2025-04-19 10:24:44 +0300

    added tests and changed some thing in the .hpp file

--- Module07/ex01/iter.hpp
+++ Module07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: mspasic <mspasic@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 20:29:53 by mspasic           #+#    #+#             */
-/*   Updated: 2025/03/26 19:35:10 by mspasic          ###   ########.fr       */
+/*   Updated: 2025/04/19 10:23:33 by mspasic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 #include <iostream>
 
-template <typename T, typename F> void iter (T *array, unsigned length, F func){
+template <typename T, typename F> void iter (T *array, size_t length, F& func){
     if (array == nullptr)
-        throw std::invalid_argument("Invalid");
-    for (unsigned i = 0; i < length; i++){
+        throw std::invalid_argument("Invalid address.");
+    for (size_t i = 0; i < length; i++){
         func(array[i]);
     }
 }
--- Module07/ex01/main.cpp
+++ Module07/ex01/main.cpp
@@ -6,26 +6,44 @@
 /*   By: mspasic <mspasic@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 20:30:04 by mspasic           #+#    #+#             */
-/*   Updated: 2025/03/26 19:38:12 by mspasic          ###   ########.fr       */
+/*   Updated: 2025/04/19 10:23:09 by mspasic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
-// #include <iostream>
 
 void print_c(char c){std::cout << c << std::endl;}
 
+void print_cc(const char c){std::cout << c << std::endl;}
+
 void print_n(int c){std::cout << c << std::endl;}
+
+void print_cn(const int c){std::cout << c << std::endl;}
 
 void add(int& x){ x += 1;}
 
 int main(void){
     {
-        char array[] = {'d', 'f', 'd', 't'};
-        int array2[] = {1, 2, 3, 4};
-        ::iter(array, 4, &print_c);
-        ::iter(array2, 4, &print_n);
-        ::iter(array2, 4, &add);
-        ::iter(array2, 4, &print_n);
+        try{
+            char array[] = {'d', 'f', 'd', 't'};
+            int array2[] = {1, 2, 3, 4};
+            std::cout << "TESTING: CHAR ARRAY:\n";
+            ::iter(array, 4, print_c);
+            std::cout << "TESTING: CHAR ARRAY (CONST):\n";
+            ::iter(array, 4, print_cc);
+            std::cout << "TESTING: INT ARRAY:\n";
+            ::iter(array2, 4, print_n);
+            std::cout << "TESTING: INT ARRAY (CONST):\n";
+            ::iter(array2, 4, print_cn);
+            std::cout << "TESTING: INT ARRAY INCREMENTED AND PRINTED:\n";
+            ::iter(array2, 4, add);
+            ::iter(array2, 4, print_n);
+            std::cout << "TESTING: NO ADDRESS:\n";
+            int *test = nullptr;
+            ::iter(test, 5, print_c);
+        }
+        catch (std::exception& e){
+            std::cout << "Error: " << e.what() << "\n";
+        }
     }
 }
