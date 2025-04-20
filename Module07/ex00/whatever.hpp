4c55f9f05e594d5c8c0f6b404f95697ccd333e91
Mils <militsaspasic@gmail.com>
2025-04-20 12:57:21 +0300

    fixed ex00

--- Module07/ex00/whatever.hpp
+++ Module07/ex00/whatever.hpp
@@ -6,28 +6,29 @@
 /*   By: mspasic <mspasic@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 20:10:49 by mspasic           #+#    #+#             */
-/*   Updated: 2025/03/26 19:07:40 by mspasic          ###   ########.fr       */
+/*   Updated: 2025/04/20 12:55:18 by mspasic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 template <typename T> void swap(T& x, T& y){
-    T temp = x;
+    T temp;
+    temp = x;
     x = y;
     y = temp;
 }
 
-template <typename T> T min(const T& x, const T& y){
+template <typename T> const T& min(const T& x, const T& y){
     // shortest: return ((y < x) ? y : x);
-    if (y <= x)
+    if (y < x)
         return y;
     return x;
 }
 
-template <typename T> T max(const T& x, const T& y){
+template <typename T> const T& max(const T& x, const T& y){
     // shortest: return ((y > x) ? y : x);
-    if (y >= x)
+    if (y > x)
         return y;
     return x;
 }
