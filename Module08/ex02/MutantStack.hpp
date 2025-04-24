c99a5627a684f71ef5343dbba51968684c339402
Milica <militsaspasic@gmail.com>
2025-04-24 21:30:38 +0300

    finished 02

--- Module08/ex02/MutantStack.hpp
+++ Module08/ex02/MutantStack.hpp
@@ -6,16 +6,56 @@
 /*   By: mspasic <mspasic@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 12:41:54 by mspasic           #+#    #+#             */
-/*   Updated: 2025/04/23 12:42:44 by mspasic          ###   ########.fr       */
+/*   Updated: 2025/04/24 19:44:21 by mspasic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
-class MutantStack{
-    private:
+#include <stack>
+
+template <class T, class Container = std::deque<T>>
+class MutantStack : public std::stack<T, Container>{
     public:
-        MutantStack();
-        ~MutantStack();
-        //...
+        //take every constructor from std::stack<T, Container>
+        //using Base::Base
+        using std::stack<T, Container>::stack;
+
+        ~MutantStack() = default;
+        MutantStack(const MutantStack& obj) = default;
+        MutantStack& operator=(const MutantStack& obj) = default;
+
+        using iterator = typename Container::iterator;
+        using const_iterator = typename Container::const_iterator;
+        using reverse_iterator = typename Container::reverse_iterator;
+        using const_reverse_iterator = typename Container::const_reverse_iterator;
+
+        iterator begin() {return this->c.begin();}
+        iterator end() {return this->c.end();}
+
+        const_iterator cbegin() {return this->c.cbegin();}
+        const_iterator cend() {return this->c.cend();}
+
+        reverse_iterator rbegin() {return this->c.rbegin();}
+        reverse_iterator rend() {return this->c.rend();}
+
+        const_reverse_iterator crbegin() {return this->c.crbegin();}
+        const_reverse_iterator crend() {return this->c.crend();}
+
+        //at()
+        auto& at(size_t i) {return this->c.at(i);}
+        const auto& at(size_t i) const {return this->c.at(i);}
+
+        //operator[]
+        auto& operator[](size_t i) {return this->c[i];}
+        const auto& operator[](size_t i) const {return this->c[i];}
+
+        //using front()
+        auto& front() {return this->c.front();}
+        const auto& front() const {return this->c.front();}
+
+        //using back()
+        auto& back() {return this->c.back();}
+        const auto& back() const {return this->c.back();}
+
 };
--- Module08/ex02/main.cpp
+++ Module08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mspasic <mspasic@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/20 14:11:17 by mspasic           #+#    #+#             */
-/*   Updated: 2025/04/23 12:40:49 by mspasic          ###   ########.fr       */
+/*   Updated: 2025/04/24 21:29:33 by mspasic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,35 +16,85 @@
 
 int main()
 {
-    MutantStack<int> mstack;
+    try{
+        MutantStack<int> mstack;
 
-    mstack.push(5);
-    mstack.push(17);
+        mstack.push(5); //first element
+        mstack.push(17);
 
-    std::cout << mstack.top() << std::endl;
+        std::cout << "Top: " << mstack.top() << std::endl; //last element in
 
-    mstack.pop();
+        mstack.pop();
 
-    std::cout << mstack.size() << std::endl;
+        std::cout << "Size: " << mstack.size() << std::endl;
 
-    mstack.push(3);
-    mstack.push(5);
-    mstack.push(737);
-    //[...]
-    mstack.push(0);
+        mstack.push(3);
+        mstack.push(5);
+        mstack.push(43);
+        mstack.push(657);
+        mstack.push(86);
+        mstack.push(24);
+        mstack.push(687);
+        mstack.push(76);
+        mstack.push(737);
+        mstack.push(0); //last element
 
-    MutantStack<int>::iterator it = mstack.begin();
-    MutantStack<int>::iterator ite = mstack.end();
-    
-    ++it;
-    --it;
-    while (it != ite)
-    {
-        std::cout << *it << std::endl;
+        MutantStack<int>::iterator it = mstack.begin();
+        MutantStack<int>::iterator ite = mstack.end();
+        
         ++it;
-    }
-    
-    std::stack<int> s(mstack);
+        --it;
+        std::cout << "Printing all values: \n";
+        while (it != ite)
+        {
+            std::cout << *it << std::endl;
+            ++it;
+        }
+        std::cout << "--------------------\n";
 
-    return 0;
+        std::stack<int> s(mstack);
+        std::cout << "S stack from mstack top element: " << s.top() << "\n";
+        std::cout << "--------------------\n";
+
+        //constructors/operator=
+        MutantStack<int> test = mstack;
+        MutantStack<int> best(test);
+
+
+        //different type iterators
+        MutantStack<int>::const_iterator c_it = mstack.cbegin();
+        std::cout << "Const iterator: " << *c_it << std::endl;
+        MutantStack<int>::reverse_iterator r_it = mstack.rbegin();
+        std::cout << "Reverse iterator: " << *r_it << std::endl;
+        MutantStack<int>::const_reverse_iterator cr_it = mstack.crbegin();
+        std::cout << "Const reverse iterator: " << *cr_it << std::endl;
+        std::cout << "--------------------\n";
+
+        //at(), should be same values
+        std::cout << "mstack.at ==> " << mstack.at(7) << "\n";
+        std::cout << "best.at ==> " << best.at(7) << "\n";
+        std::cout << "test.at ==> " << test.at(7) << "\n";
+        std::cout << "--------------------\n";
+
+        //[](), should be same values
+        std::cout << "mstack[] ==> " << mstack[3] << "\n";
+        std::cout << "best[] ==> " << best[3] << "\n";
+        std::cout << "test[] ==> " << test[3] << "\n";
+        std::cout << "--------------------\n";
+        
+        //front(), should be same values
+        std::cout << "mstack.front ==> " << mstack.front() << "\n";
+        std::cout << "best.front ==> " << best.front() << "\n";
+        std::cout << "test.front ==> " << test.front() << "\n";
+        std::cout << "--------------------\n";
+
+        //back(), should be same values
+        std::cout << "mstack.back ==> " << mstack.back() << "\n";
+        std::cout << "best.back ==> " << best.back() << "\n";
+        std::cout << "test.back ==> " << test.back() << "\n";
+        std::cout << "--------------------\n";
+    }
+    catch(const std::exception& e){
+        std::cout << "Error: " << e.what();
+    }
 }
