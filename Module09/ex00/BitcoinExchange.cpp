366d9df9d9fe92aa986597bf74341b76c29ffce9
Milica <militsaspasic@gmail.com>
2025-10-02 08:26:40 +0300

    i think i finished ex00, test and clean up

--- Module09/ex00/BitcoinExchange.cpp
+++ Module09/ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: mspasic <mspasic@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 21:47:09 by mspasic           #+#    #+#             */
-/*   Updated: 2025/10/02 01:21:47 by mspasic          ###   ########.fr       */
+/*   Updated: 2025/10/02 08:26:12 by mspasic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,28 +41,28 @@ int BitcoinExchange::getDate(const std::string& date){
     int year, month, day;
 
     if (spaces(date) == true)
-        throw std::invalid_argument("a date contains spaces: " + date);
+        throw std::invalid_argument("bad input => " + date);
 
     if (sscanf(date.c_str(), "%d-%d-%d", &year, &month, &day) != 3)
-        throw std::invalid_argument("wrong date format: " + date);
+        throw std::invalid_argument("wrong date format => " + date);
 
-    if ( 2009 > year || 2025 < year)
-        throw std::invalid_argument("year value invalid: " + date);
+    if ( 1 > year || 2025 < year)
+        throw std::invalid_argument("year value invalid => " + date);
 
     if ( 1 > month || 12 < month)
-        throw std::invalid_argument("month value invalid: " + date);
+        throw std::invalid_argument("month value invalid => " + date);
 
     if ( 1 > day || 31 < day)
-        throw std::invalid_argument("day value invalid: " + date);
+        throw std::invalid_argument("day value invalid => " + date);
 
     // february during leap year
     if (month == 2){
         if ((day > 29) || (isLeapYear(year) == true && day > 30) || (isLeapYear(year) == false && day > 29))
-            throw std::invalid_argument("date invalid: " + date);
+            throw std::invalid_argument("date invalid => " + date);
     }
     
     if (day > 30 && (month == 4 || month == 6 || month == 9 || month == 11))
-        throw std::invalid_argument("day value invalid: " + date);
+        throw std::invalid_argument("day value invalid => " + date);
     
     return (year * 10000 + month * 100 + day);
 }
@@ -72,7 +72,7 @@ void BitcoinExchange::processFile(const std::string& input_file, unsigned char c
     //checking whether the file exists and can be opened
     std::fstream data(input_file, std::ios::in);
     if (!data.is_open())
-        throw std::ios_base::failure("file could not be opened");
+        throw std::ios_base::failure("could not open file");
 
     //set the first line
     std::string firstline;
@@ -92,7 +92,7 @@ void BitcoinExchange::processFile(const std::string& input_file, unsigned char c
     std::string line;
     std::getline(data, line);
     if (line != firstline)
-        throw std::invalid_argument("invalid file content");
+        throw std::invalid_argument("invalid file content  => " + line);
 
     std::string date;
     std::string rate;
@@ -107,7 +107,7 @@ void BitcoinExchange::processFile(const std::string& input_file, unsigned char c
             content.append(line);
             auto pos = line.find(c);
             if (pos == std::string::npos)
-                throw std::invalid_argument("invalid file content");
+                throw std::invalid_argument("bad input => " + line);
             if (inputed == false){
                 date = line.substr(0, pos);
                 rate = line.substr(pos + 1);
@@ -121,20 +121,41 @@ void BitcoinExchange::processFile(const std::string& input_file, unsigned char c
             try{
                 date_conv = getDate(date);
                 rate_conv = std::stod(rate, &int_addr);
-                if (int_addr != rate.length() || (rate_conv < 0))
-                    throw std::invalid_argument("invalid data.csv content");
+                if (int_addr != rate.length())
+                    throw std::invalid_argument("bad input => " + rate);
+                //a valid rate value must be a positive number
+                if  (rate_conv < 0)
+                    throw std::invalid_argument("not a positive number.");   
                 if (inputed == false)
                     rates[date_conv] = rate_conv;
                 else{
                     //uporedi date, izracunaj rate, isprintaj
-                    
+                    //a valid rate value must be between 0 and 1000
+                    if (rate_conv > 1000)
+                        throw std::invalid_argument("too large a number.");
+                    // returns an iter to the first element whose key is >= value
+                    auto rate_year = rates.lower_bound(date_conv);
+                    //1st case: exact match was found
+                    if (rate_year != rates.end() && rate_year->first == date_conv){
+                        std::cout << date << " => " << rate_year->second << " = " << rate_year->second * rate_conv << "\n"; 
+                        continue;
+                    }
+                    //2nd case: no exact match, but earlier date exists
+                    else if (rate_year != rates.begin()){
+                        rate_year--;
+                        std::cout << date << " => " << rate_year->second << " = " << rate_year->second * rate_conv << "\n"; 
+                        continue;
+                    }
+                    else{
+                        throw std::out_of_range("rate unavailable for date => " + date);
+                    }
                 } 
             } catch (std::exception& e){
-                std::cout << "Error: " << e.what();
+                std::cout << "Error: " << e.what() << std::endl;
             }
         }
         else{
-            throw std::invalid_argument("file too big");    
+            throw std::invalid_argument("file invalid");    
         }  
     }
     data.close();
